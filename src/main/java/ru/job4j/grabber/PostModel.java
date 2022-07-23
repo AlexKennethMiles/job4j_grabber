0886cb564c48f6bb18b3418a71c9458f65c9f84a
AlexKennethMiles <apuchkov17@gmail.com>
2022-07-23 16:19:55 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/PostModel.java
@@ -0,0 +1,97 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class PostModel {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public PostModel(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public PostModel(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PostModel postModel = (PostModel) o;
+        return id == postModel.id
+                && Objects.equals(title, postModel.title)
+                && Objects.equals(link, postModel.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link);
+    }
+
+    @Override
+    public String toString() {
+        return "PostModel{"
+                + " id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
